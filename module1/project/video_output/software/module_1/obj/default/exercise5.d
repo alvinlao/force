bc7eedb990ce1099886529dd2fddd6bb15623e1f
treyu <trevor.tyu@gmail.com>
2015-01-24 20:16:18 -0800

    Initial Commit for the Video Output
    - Added custom hardware components for video out to the Qsys project from
      Exercise 1.5.  Code just draws rectangles of random size, position, and
      colour on the monitor.
    - Not committing the entire project, but only the ones that seem
      important (will clean up later).
    - Project will NOT work for Nios II/s and II/f processors because they
      take up too much memory.
    - Tried to add SDRAM to the project, but could not get it working.

---
+++ module1/project/video_output/software/module_1/obj/default/exercise5.d
@@ -0,0 +1,28 @@
+obj/default/exercise5.o: exercise5.c ../module_1_bsp//HAL/inc/io.h \
+  ../module_1_bsp//HAL/inc/alt_types.h \
+  ../module_1_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+  ../module_1_bsp//HAL/inc/alt_types.h \
+  ../module_1_bsp//HAL/inc/sys/alt_dev.h ../module_1_bsp/system.h \
+  ../module_1_bsp/linker.h ../module_1_bsp//HAL/inc/sys/alt_llist.h \
+  ../module_1_bsp//HAL/inc/priv/alt_dev_llist.h \
+  ../module_1_bsp//HAL/inc/sys/alt_llist.h
+
+../module_1_bsp//HAL/inc/io.h:
+
+../module_1_bsp//HAL/inc/alt_types.h:
+
+../module_1_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+../module_1_bsp//HAL/inc/alt_types.h:
+
+../module_1_bsp//HAL/inc/sys/alt_dev.h:
+
+../module_1_bsp/system.h:
+
+../module_1_bsp/linker.h:
+
+../module_1_bsp//HAL/inc/sys/alt_llist.h:
+
+../module_1_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../module_1_bsp//HAL/inc/sys/alt_llist.h:
