237a9baae50234c784a83486e359f76482840c41
treyu <trevor.tyu@gmail.com>
2015-01-26 19:09:26 -0800

    Big commit (never doing this again)... maybe
    - Removed all the existing files and replaced them with only the source
      code files.
    - Added awesomeness to the VHDL code (Jae did it).
    - What works? Nothing really.

--- module1/project/video_output/vga_graphics.vhd
+++ module1/project/video_output/vga_graphics.vhd
@@ -4,6 +4,8 @@ use ieee.std_logic_unsigned.all;
 
 entity vga_graphics is
 	port (
+	GPIO_1 : out std_LOGIC;
+		LEDG : OUT STD_LOGIC_VECTOR(7 downto 0);
 		KEY : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
 		CLOCK_50 : IN STD_LOGIC;
 		CLOCK_27 : IN STD_LOGIC;
@@ -22,12 +24,12 @@ entity vga_graphics is
 		SRAM_CE_N : OUT STD_LOGIC;
 		SRAM_OE_N : OUT STD_LOGIC;
 		SRAM_WE_N : OUT STD_LOGIC;
-		DRAM_CLK, DRAM_CKE : OUT STD_LOGIC;
-		DRAM_ADDR : OUT STD_LOGIC_VECTOR(11 DOWNTO 0);
-		DRAM_BA_0, DRAM_BA_1 : BUFFER STD_LOGIC;
-		DRAM_CS_N, DRAM_CAS_N, DRAM_RAS_N, DRAM_WE_N : OUT STD_LOGIC;
-		DRAM_DQ : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-		DRAM_UDQM, DRAM_LDQM : BUFFER STD_LOGIC;
+--		DRAM_CLK, DRAM_CKE : OUT STD_LOGIC;
+--		DRAM_ADDR : OUT STD_LOGIC_VECTOR(11 DOWNTO 0);
+--		DRAM_BA_0, DRAM_BA_1 : BUFFER STD_LOGIC;
+--		DRAM_CS_N, DRAM_CAS_N, DRAM_RAS_N, DRAM_WE_N : OUT STD_LOGIC;
+--		DRAM_DQ : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+--		DRAM_UDQM, DRAM_LDQM : BUFFER STD_LOGIC;
 		TD_DATA : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
 		TD_HS : IN STD_LOGIC;
 		TD_VS : IN STD_LOGIC
@@ -45,15 +47,16 @@ architecture structural of vga_graphics is
 			sram_CE_N : OUT STD_LOGIC;
 			sram_OE_N : OUT STD_LOGIC;
 			sram_WE_N : OUT STD_LOGIC;
-			sdram_wire_addr : OUT STD_LOGIC_VECTOR(11 DOWNTO 0);
-			sdram_wire_ba : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
-			sdram_wire_cas_n : OUT STD_LOGIC;
-			sdram_wire_cke : OUT STD_LOGIC;
-			sdram_wire_cs_n : OUT STD_LOGIC;
-			sdram_wire_dq : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-			sdram_wire_dqm : BUFFER STD_LOGIC_VECTOR(1 DOWNTO 0);
-			sdram_wire_ras_n : OUT STD_LOGIC;
-			sdram_wire_we_n : OUT STD_LOGIC;
+--			sdram_clk_clk : OUT STD_LOGIC;
+--			sdram_wire_addr : OUT STD_LOGIC_VECTOR(11 DOWNTO 0);
+--			sdram_wire_ba : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+--			sdram_wire_cas_n : OUT STD_LOGIC;
+--			sdram_wire_cke : OUT STD_LOGIC;
+--			sdram_wire_cs_n : OUT STD_LOGIC;
+--			sdram_wire_dq : INOUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+--			sdram_wire_dqm : BUFFER STD_LOGIC_VECTOR(1 DOWNTO 0);
+--			sdram_wire_ras_n : OUT STD_LOGIC;
+--			sdram_wire_we_n : OUT STD_LOGIC;
 			vga_controller_CLK : OUT STD_LOGIC;
 			vga_controller_HS : OUT STD_LOGIC;
 			vga_controller_VS : OUT STD_LOGIC;
@@ -62,7 +65,8 @@ architecture structural of vga_graphics is
 			vga_controller_R : OUT STD_LOGIC_VECTOR(9 downto 0);
 			vga_controller_G : OUT STD_LOGIC_VECTOR(9 downto 0);
 			vga_controller_B : OUT STD_LOGIC_VECTOR(9 downto 0);
-			v_in_position_export : IN STD_LOGIC_VECTOR(16 DOWNTO 0);
+			v_in_position_x_export : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
+			v_in_position_y_export : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
 			v_in_rgb_export : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
 			v_in_pixel_enabled_export : IN STD_LOGIC
 		);
@@ -105,10 +109,10 @@ architecture structural of vga_graphics is
 	signal BA : STD_LOGIC_VECTOR(1 DOWNTO 0);
 	
 begin
-	DRAM_BA_0 <= BA(0);
-	DRAM_BA_1 <= BA(1);
-	DRAM_UDQM <= DQM(1);
-	DRAM_LDQM <= DQM(0);
+--	DRAM_BA_0 <= BA(0);
+--	DRAM_BA_1 <= BA(1);
+--	DRAM_UDQM <= DQM(1);
+--	DRAM_LDQM <= DQM(0);
 	
 NiosII: nios_system
 	port map (
@@ -129,26 +133,28 @@ NiosII: nios_system
 		sram_CE_N => SRAM_CE_N,
 		sram_OE_N => SRAM_OE_N,
 		sram_WE_N => SRAM_WE_N,
-		sdram_wire_addr => DRAM_ADDR,
-		sdram_wire_ba => BA,
-		sdram_wire_cas_n => DRAM_CAS_N,
-		sdram_wire_cke => DRAM_CKE,
-		sdram_wire_cs_n => DRAM_CS_N,
-		sdram_wire_dq => DRAM_DQ,
-		sdram_wire_dqm => DQM,
-		sdram_wire_ras_n => DRAM_RAS_N,
-		sdram_wire_we_n => DRAM_WE_N,
-		v_in_position_export => position,
+--		sdram_clk_clk => DRAM_CLK,
+--		sdram_wire_addr => DRAM_ADDR,
+--		sdram_wire_ba => BA,
+--		sdram_wire_cas_n => DRAM_CAS_N,
+--		sdram_wire_cke => DRAM_CKE,
+--		sdram_wire_cs_n => DRAM_CS_N,
+--		sdram_wire_dq => DRAM_DQ,
+--		sdram_wire_dqm => DQM,
+--		sdram_wire_ras_n => DRAM_RAS_N,
+--		sdram_wire_we_n => DRAM_WE_N,
+		v_in_position_x_export => position_x,
+		v_in_position_y_export => position_y,
 		v_in_rgb_export => rgb,
 		v_in_pixel_enabled_export => en
 	);
-	--DRAM_CLK <= CLOCK_50;
+--	DRAM_CLK <= CLOCK_50;
 
 VI : video_in	
 	port map (
 		CLOCK_50 => CLOCK_50,
 		CLOCK_27 => CLOCK_27,
-		reset => KEY(0),
+		reset => not KEY(0),
 
 		TD_DATA => TD_DATA,
 		TD_HS => TD_HS,
@@ -167,7 +173,8 @@ VI : video_in
 		pixel_en => en
 	);
 	
-	position <= position_x & position_y;
 	rgb <= red & green & blue;
 	waitrequest <= '0';
+	LEDG <= en & "0001111";
+	GPIO_1 <= en;
 end structural;
--- module1/project/video_output/video_output.c
+++ module1/project/video_output/video_output.c
@@ -1,32 +1,92 @@
 #include <stdio.h>
 #include "io.h"
 #include <stdlib.h>
+#include <time.h>
 #include "altera_up_avalon_video_pixel_buffer_dma.h"
-#define drawer_base (volatile int *) 0x7020
+#include "altera_up_avalon_video_character_buffer_with_dma.h"
+#include <unistd.h>
+#define drawer_base (volatile int *) 0xb020
 
 int main()
 {
 	alt_up_pixel_buffer_dma_dev* pixel_buffer;
-	pixel_buffer = alt_up_pixel_buffer_dma_open_dev("/dev/video_pixel_buffer_dma");
+	pixel_buffer = alt_up_pixel_buffer_dma_open_dev("/dev/pixel_buffer_dma");
 	if (pixel_buffer == 0) {
-		printf("error initializing pixel buffer (check name in alt_up_pixel_buffer_dma_open_dev)\n");
+		//printf("error initializing pixel buffer (check name in alt_up_pixel_buffer_dma_open_dev)\n");
 	}
-	alt_up_pixel_buffer_dma_change_back_buffer_address(pixel_buffer, PIXEL_BUFFER_BASE);
-	alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer);
-	while (alt_up_pixel_buffer_dma_check_swap_buffers_status(pixel_buffer));
-	alt_up_pixel_buffer_dma_clear_screen(pixel_buffer, 0);
 
-	int hw = 0;
-	if (hw) {
-           IOWR_32DIRECT(drawer_base,0,10); // Set x1
-           IOWR_32DIRECT(drawer_base,4,20); // Set y1
-           IOWR_32DIRECT(drawer_base,8,50); // Set x2
-           IOWR_32DIRECT(drawer_base,12,60); // Set y2
-           IOWR_32DIRECT(drawer_base,16,0xFFFF);  // Set colour
-           IOWR_32DIRECT(drawer_base,20,1);  // Start drawing
-           while(IORD_32DIRECT(drawer_base,20)==0); // wait until done
- 	} else {
-           alt_up_pixel_buffer_dma_draw_box(pixel_buffer, 10,20,50,60,0xFFFF,0);
- 	}
+	while(1) {
+		int x1, x2, y1, y2, colour, max_x, max_y;
+		max_x = 20;
+		max_y = 20;
+		x1 = rand() % (320 - max_x);
+		x2 = (rand() % max_x) + x1;
+		y1 = rand() % (240 - max_y);
+		y2 = (rand() % max_y) + y1;
+		colour = rand() % 65535;
+
+		alt_up_pixel_buffer_dma_change_back_buffer_address(pixel_buffer, PIXEL_BUFFER_BASE);
+		alt_up_pixel_buffer_dma_swap_buffers(pixel_buffer);
+		while (alt_up_pixel_buffer_dma_check_swap_buffers_status(pixel_buffer));
+//		alt_up_pixel_buffer_dma_clear_screen(pixel_buffer, 0);
+
+//		   IOWR_32DIRECT(drawer_base,0,0); // Set x1
+//		   IOWR_32DIRECT(drawer_base,4,0); // Set y1
+//		   IOWR_32DIRECT(drawer_base,8,320); // Set x2
+//		   IOWR_32DIRECT(drawer_base,12,240); // Set y2
+//		   IOWR_32DIRECT(drawer_base,16,0xffff);  // Set blue
+//		   IOWR_32DIRECT(drawer_base,20,1);  // Start drawing
+//		   while(IORD_32DIRECT(drawer_base,20)==0); // wait until done
+
+		char *pixelEn = 0xb090;
+		int *rgb = 0xb0a0;
+		int *positionX = 0xb0b0;
+		int *positionY = 0xb080;
+
+
+
+		int hw = 0;
+		if (*pixelEn > 1) {
+			alt_up_char_buffer_dev *char_buffer;
+			char_buffer = alt_up_char_buffer_open_dev("/dev/char_drawer");
+			alt_up_char_buffer_init(char_buffer);
+			alt_up_char_buffer_string(char_buffer, *positionX, 100, 100);
+//			printf(*positionX);
+			if (hw) {
+	//				IOWR_32DIRECT(drawer_base,0,x1); // Set x1
+	//				IOWR_32DIRECT(drawer_base,4,y1); // Set y1
+	//				IOWR_32DIRECT(drawer_base,8,x2); // Set x2
+	//				IOWR_32DIRECT(drawer_base,12,y2); // Set y2
+	//				IOWR_32DIRECT(drawer_base,16,colour);  // Set blue
+	//				IOWR_32DIRECT(drawer_base,20,1);  // Start drawing
+				   IOWR_32DIRECT(drawer_base,0,*positionX); // Set x1
+				   IOWR_32DIRECT(drawer_base,4,*positionY); // Set y1
+				   IOWR_32DIRECT(drawer_base,8,*positionX); // Set x2
+				   IOWR_32DIRECT(drawer_base,12,*positionY); // Set y2
+				   IOWR_32DIRECT(drawer_base,16,*rgb);  // Set blue
+				   IOWR_32DIRECT(drawer_base,20,1);  // Start drawing
+				   while(IORD_32DIRECT(drawer_base,20)==0); // wait until done
+			} else {
+	//			   alt_up_pixel_buffer_dma_draw_box(pixel_buffer, x1,y1,x2,y2,colour,0);
+				alt_up_pixel_buffer_dma_draw(pixel_buffer, 0xffff, 10, 10);
+				alt_up_pixel_buffer_dma_draw(pixel_buffer, *rgb, *positionX, *positionY);
+			}
+		}
+//		int i = 0;
+//		int j = 0;
+//		for (i = 0; i < 160; i++) {
+//			for (j = 0; j < 120; j++) {
+//			   IOWR_32DIRECT(drawer_base,0,i); // Set x1
+//			   IOWR_32DIRECT(drawer_base,4,j); // Set y1
+//			   IOWR_32DIRECT(drawer_base,8,i); // Set x2
+//			   IOWR_32DIRECT(drawer_base,12,j); // Set y2
+//			   IOWR_32DIRECT(drawer_base,16,0xffff);  // Set blue
+//			   IOWR_32DIRECT(drawer_base,20,1);  // Start drawing
+//			   while(IORD_32DIRECT(drawer_base,20)==0); // wait until done
+////				alt_up_pixel_buffer_dma_draw(pixel_buffer, 0xffff, i, j);
+//			}
+//		}
+	}
     return 0;
 }
+
