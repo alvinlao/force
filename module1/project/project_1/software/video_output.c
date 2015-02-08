221b0290be3de5f872d7072931f3f0b2608f9e8a
Alvin <alvin.lao.is@gmail.com>
2015-02-07 18:45:07 -0800

    line in software

--- module1/project/project_1/software/video_output.c
+++ module1/project/project_1/software/video_output.c
@@ -7,6 +7,7 @@
 #include "sad.h"
 
 #define drawer_base (volatile int *) 0xb020
+#define draw_base (volatile int *) 0x00089400
 #define UP 1
 #define DOWN 2
 #define RIGHT 3
@@ -30,6 +31,7 @@ alt_up_pixel_buffer_dma_dev * initPixelBuffer();
 Box * initBox(int x, int y, int w, int h, int s, int c);
 void updateBox(Box *);
 void drawBox(alt_up_pixel_buffer_dma_dev *, Box *);
+void plotLine(alt_up_pixel_buffer_dma_dev* pixel_buffer, int x0, int y0, int x1, int y1, int color, int back_buffer);
 
 
 alt_up_pixel_buffer_dma_dev* initPixelBuffer() {
@@ -106,14 +108,49 @@ void updateBox(Box *box) {
 }
 
 void drawBox(alt_up_pixel_buffer_dma_dev* pixel_buffer, Box *box) {
-	alt_up_pixel_buffer_dma_draw_box(pixel_buffer, box->x1, box->y1, box->x2, box->y2, box->color, 1);
+	IOWR_32DIRECT(draw_base, 0, 10);
+	IOWR_32DIRECT(draw_base, 4, 20);
+	IOWR_32DIRECT(draw_base, 8, 50);
+	IOWR_32DIRECT(draw_base, 12, 60);
+	IOWR_32DIRECT(draw_base, 16, 0xffff);
+	IOWR_32DIRECT(draw_base, 20, 1);
+	while(IORD_32DIRECT(draw_base, 20) == 0);
+//	alt_up_pixel_buffer_dma_draw_box(pixel_buffer, box->x1, box->y1, box->x2, box->y2, box->color, 1);
+}
+
+void setPixel(int buffer_start, int line_size, int x, int y, int color) {
+	helper_plot_pixel(buffer_start, line_size, x, y, color, 1);
+}
+
+void plotLine(alt_up_pixel_buffer_dma_dev* pixel_buffer, int x0, int y0, int x1, int y1, int color, int backbuffer)
+{
+	int buffer_start;
+	int color_mode = 1;
+	register int line_size = (pixel_buffer->addressing_mode == ALT_UP_PIXEL_BUFFER_XY_ADDRESS_MODE) ? (1 << (pixel_buffer->y_coord_offset-color_mode)) : pixel_buffer->x_resolution;
+
+	int dx =  abs(x1-x0), sx = x0<x1 ? 1 : -1;
+	int dy = -abs(y1-y0), sy = y0<y1 ? 1 : -1;
+	int err = dx+dy, e2;  // error value e_xy
+
+	if (backbuffer == 1)
+		buffer_start = pixel_buffer->back_buffer_start_address;
+	else
+		buffer_start = pixel_buffer->buffer_start_address;
+
+	for(;;) {
+		setPixel(buffer_start, line_size, x0, y0, color);
+		if (x0==x1 && y0==y1) break;
+		e2 = 2*err;
+		if (e2 >= dy) { err += dy; x0 += sx; } // e_xy+e_x > 0;
+		if (e2 <= dx) { err += dx; y0 += sy; } // e_xy+e_y < 0;
+   }
 }
 
 int main()
 {
 	// Setup pixel buffer
 	alt_up_pixel_buffer_dma_dev* pixel_buffer = initPixelBuffer();
-
+/*
 	// Setup tracking algorithm
 	Block *targetBlock, *resultBlock, *window;
 	Pixel *pixelA, *pixelB;
@@ -128,12 +165,12 @@ int main()
 	// Draw first frame and copy track block into memory
 	drawBox(pixel_buffer, box);
 	VideoCopyBlock(BOX_X0, BOX_Y0, pixelA);
-
+*/
 	// Spam
-	printf("Window: (%d, %d)\n", BlockGetX(window), BlockGetY(window));
-	printf("Block: (%d, %d)\n", BlockGetX(targetBlock), BlockGetY(targetBlock));
-	printf("Front buffer addr: %x\n", pixel_buffer->buffer_start_address);
-	printf("Back buffer addr: %x\n", pixel_buffer->back_buffer_start_address);
+//	printf("Window: (%d, %d)\n", BlockGetX(window), BlockGetY(window));
+//	printf("Block: (%d, %d)\n", BlockGetX(targetBlock), BlockGetY(targetBlock));
+//	printf("Front buffer addr: %x\n", pixel_buffer->buffer_start_address);
+//	printf("Back buffer addr: %x\n", pixel_buffer->back_buffer_start_address);
 
 
 	printf("Here we go...\n");
@@ -149,18 +186,23 @@ int main()
 //		while (alt_up_pixel_buffer_dma_check_swap_buffers_status(pixel_buffer));
 
 		// Apply algorithm
-		SADTrack(targetBlock, resultBlock, window, pixelA, pixelB);
-
-		int track_x = BlockGetX(resultBlock);
-		int track_y = BlockGetY(resultBlock);
-		printf("Block: (%d, %d)\n", track_x, track_y);
-
-		// Draw marker
-		marker->x1 = track_x;
-		marker->y1 = track_y;
-		marker->x2 = track_x + MARKER_SIZE;
-		marker->y2 = track_y + MARKER_SIZE;
-		drawBox(pixel_buffer, marker);
+//		SADTrack(targetBlock, resultBlock, window, pixelA, pixelB);
+//
+//		int track_x = BlockGetX(resultBlock);
+//		int track_y = BlockGetY(resultBlock);
+//		printf("Block: (%d, %d)\n", track_x, track_y);
+//
+//		// Draw marker
+//		marker->x1 = track_x;
+//		marker->y1 = track_y;
+//		marker->x2 = track_x + MARKER_SIZE;
+//		marker->y2 = track_y + MARKER_SIZE;
+		drawBox(pixel_buffer, 0);
+		plotLine(pixel_buffer, 0, FRAME_HEIGHT/2, FRAME_WIDTH, FRAME_HEIGHT/2, 0xffff, 1);
+		plotLine(pixel_buffer, FRAME_WIDTH/2, 0, FRAME_WIDTH/2, FRAME_HEIGHT, 0, 1);
+		plotLine(pixel_buffer, 0, 0, FRAME_WIDTH, FRAME_HEIGHT, 0xffff, 1);
+		plotLine(pixel_buffer, 0, FRAME_HEIGHT, FRAME_WIDTH, 0, 0xffff, 1);
+//		drawBox(pixel_buffer, marker);
 	}
 
     return 0;
