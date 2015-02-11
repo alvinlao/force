d450550abc805b6afa43f1029bc2a56ff17f8f50
Alvin <alvin.lao.is@gmail.com>
2015-02-11 00:07:03 -0800

    Simplify SAD block

--- module1/project/project_1/hardware/project_1_test_block.vhd
+++ module1/project/project_1/hardware/project_1_test_block.vhd
@@ -9,7 +9,9 @@ use work.my_types.all;
 entity project_1_test_block is
 	generic (
 		sizeX : integer := 3;
-		sizeY : integer := 3
+		sizeY : integer := 3;
+		winSizeX : integer := 8;
+		winSizeY : integer := 8
 	);
 	port (
 		SW : IN std_logic_vector(17 downto 0);
@@ -22,58 +24,59 @@ end project_1_test_block;
 architecture Behavioural of project_1_test_block is	
 	component sad_block
 	generic (
-		sizeX : integer;
-		sizeY : integer
+		sizeX, sizeY : integer;
+		winSizeX, winSizeY : integer
 	);
 	port (
 		reset	: in std_logic;
 		clock	: in std_logic;
 		
 		ready	: out std_logic;
-		ready_block : out std_logic;
-		done	: out std_logic;
 		start	: in std_logic;
-		start_block : in std_logic;
-		block_read_en : in std_logic;
+		done	: out std_logic;
 
-		ref_block : in BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
-		can_block : in BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
+		-- Absolute x, y offsets
+		x0			: in integer;
+		y0			: in integer;
+		
+		window : in BlockType(winSizeX-1 downto 0, winSizeY-1 downto 0);
+		ref : in BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
 		
 		sad : out unsigned(31 downto 0)
 	);
 	end component;
 
 	signal ready : std_logic;
-	signal ready_block : std_logic;
 	signal done : std_logic;
 	
 	signal output : unsigned(31 downto 0);	
 	
-	signal ref_block, can_block : BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
+	signal window : BlockType(winSizeX-1 downto 0, winSizeY-1 downto 0);
+	signal ref : BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
+	signal x0 : integer := 0;
+	signal y0 : integer := 0;
 	
 begin
-	LEDR(0) <= ready;
-	LEDR(1) <= ready_block;
-	LEDR(2) <= done;
-	
 	SB : sad_block 
 		generic map (
 			sizeX => sizeX,
-			sizeY => sizeY
+			sizeY => sizeY,
+			winSizeX => winSizeX,
+			winSizeY => winSizeY
 		)
 		port map (
 			reset => not KEY(0),
 			clock => KEY(1),
 			
 			ready => ready,
-			ready_block => ready_block,
 			done => done,
 			start => SW(0),
-			start_block => SW(1),
 			
-			ref_block => ref_block,
-			can_block => can_block,
-			block_read_en => SW(17),
+			x0 => x0,
+			y0 => y0,
+			
+			window => window,
+			ref => ref,		
 			
 			sad => output
 		);
@@ -81,16 +84,27 @@ begin
 	process(KEY(1))
 	begin
 		if(rising_edge(KEY(1))) then 
-			for i in 0 to sizeX-1 loop
-				for j in 0 to sizeY-1 loop
-					can_block(i, j) <= b"0000_0000_0000_0000";
+		
+			for i in 0 to winSizeX-1 loop
+				for j in 0 to winSizeY-1 loop
+					window(i, j) <= b"0000_0000_0000_0000";
 					if(i = 0) and (j = 0) then
-						can_block(i, j) <= b"0000_0000_0000_0011";
+						window(i, j) <= b"0000_0000_0000_0001";
 					end if;
 				end loop;
 			end loop;
+			
+			for i in 0 to sizeX-1 loop
+				for j in 0 to sizeY-1 loop
+					ref(i, j) <= b"0000_0000_0000_0000";
+				end loop;
+			end loop;
+			
 		end if;
 	end process;
 	
+	LEDR(0) <= ready;
+	LEDR(1) <= done;
 	LEDG(7 downto 0) <= std_logic_vector(output)(7 downto 0);
+	
 end Behavioural;
--- module1/project/project_1/hardware/sad_block.vhd
+++ module1/project/project_1/hardware/sad_block.vhd
@@ -11,7 +11,9 @@ use work.my_types.all;
 entity sad_block is
 	generic (
 		sizeX : integer := 3;
-		sizeY : integer := 3
+		sizeY : integer := 3;
+		winSizeX : integer := 8;
+		winSizeY : integer := 8
 	);
 	
 	port (
@@ -19,14 +21,16 @@ entity sad_block is
 		clock	: in std_logic;
 		
 		ready	: out std_logic;
-		ready_block : out std_logic;
-		done	: out std_logic;
 		start	: in std_logic;
-		start_block : in std_logic;
-		block_read_en : in std_logic;
+		done	: out std_logic;
 
-		ref_block : in BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
-		can_block : in BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
+		-- Absolute x, y offsets
+		x0			: in integer;
+		y0			: in integer;
+		
+		-- Blocks
+		window : in BlockType(winSizeX-1 downto 0, winSizeY-1 downto 0);
+		ref : in BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
 		
 		sad : out unsigned(31 downto 0)
 	);
@@ -41,19 +45,17 @@ architecture Behavioural of sad_block is
 	);
 	end component;
 	
-	type state is (IDLE, LOADREF, IDLEBLOCK, LOADBLOCK, SADPIXEL, SUMSAD, FINISH);
+	type state is (IDLE, ADD_SAD, FINISH);
 	signal current_state : state := IDLE;
-
-	signal can : BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
-	signal ref : BlockType(sizeX-1 downto 0, sizeY-1 downto 0);
+	
 	signal sads : SADBlockType(sizeX-1 downto 0, sizeY-1 downto 0);
 	
 begin
 	-- One SAD_pixel for each pixel
 	GX: for i in 0 to sizeX-1 generate
 		GY: for j in 0 to sizeY-1 generate
-			SP: sad_pixel port map(
-				ref_rgb => can(i, j),
+			SP: sad_pixel port map (
+				ref_rgb => window(x0 + i, y0 + j),
 				can_rgb => ref(i, j),
 				sad 	=> sads(i, j)
 			);
@@ -69,58 +71,35 @@ begin
 		if(reset = '1') then
 			current_state <= IDLE;
 		elsif(rising_edge(clock)) then
-			done <= '0';
 			ready <= '0';
-			ready_block <= '0';
-			
+			done <= '0';
+				
 			case current_state is
 				when IDLE =>
 					ready <= '1';
 					sad_var := b"0000_0000_0000_0000_0000_0000_0000_0000";
-					sad <= sad_var;
 					
 					if(start = '1') then
-						current_state <= LOADREF;
+						current_state <= ADD_SAD;
 					end if;
-				when LOADREF =>
-					if(block_read_en = '1') then
-						ref <= ref_block;
-						current_state <= IDLEBLOCK;
-					end if;
-				when IDLEBLOCK =>
-					ready_block <= '1';
-					sad_var := b"0000_0000_0000_0000_0000_0000_0000_0000";
-					sad <= sad_var;
-					
-					if(start_block = '1') then
-						current_state <= LOADBLOCK;
-					end if;
-				when LOADBLOCK =>
-					if(block_read_en = '1') then
-						can <= can_block;
-						current_state <= SADPIXEL;
-					end if;
-				when SADPIXEL =>
-					-- Done instantly
-					current_state <= SUMSAD;
-				when SUMSAD =>
+				when ADD_SAD =>
+					-- sads contain the sad for the pixel at that location
 					for i in 0 to sizeX-1 loop
 						for j in 0 to sizeY-1 loop
 							sad_var := sad_var + sads(i, j);
 						end loop;
 					end loop;
 					
-					sad <= sad_var;
 					current_state <= FINISH;
 				when FINISH =>
 					done <= '1';
 					
-					if(start_block = '0') then
-						current_state <= IDLEBLOCK;
-					elsif (start = '0') then 
+					if (start = '0') then 
 						current_state <= IDLE;
 					end if;
 			end case;
+			
+			sad <= sad_var;
 		end if;
 	end process;
 	
