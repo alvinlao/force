519cbc773d0b8e2c18a1bef392cfaed7793ef4f4
Alvin Lao <alvin.lao.is@gmail.com>
2015-03-11 23:26:01 -0700

    Remove unneeded PointModel class

--- module2/project/software/filters/kalmanfilter/src/KalmanFilter.java
+++ module2/project/software/filters/kalmanfilter/src/KalmanFilter.java
@@ -2,7 +2,12 @@
  * Filter noise from measurements
  */
 public class KalmanFilter {
-    PointModel model;
+    public static final int MODEL_NOISE = 0;
+
+    // [0, 1] Predict how much error our model has
+    public static final float PREDICTION_ERROR = 0.05f;
+
+    PointState state;
 
     public KalmanFilter() {
         // Create our model
@@ -10,13 +15,58 @@ public class KalmanFilter {
         int initialY = Frame.HEIGHT / 2;
         float initialError = 0.3f;
 
-        model = new PointModel(initialX, initialY, initialError);
+        state = new PointState();
+
+        state.x = initialX;
+        state.y = initialY;
+        state.errorX = initialError;
+        state.errorY = initialError;
     }
 
     public Point run(Measurement measurement) {
-        model.predict();
-        model.update(measurement);
+        predict();
+        update(measurement);
 
-        return model.state;
+        return state;
+    }
+
+    /**
+     * Make a prediction of what happens next
+     */
+    public void predict() {
+        // Predict the object stays in the same place
+        float stepX = 1;
+        float stepY = 1;
+
+        // Generate noise
+        double noiseX = (2 * Math.random() - 1) * MODEL_NOISE;
+        double noiseY = (2 * Math.random() - 1) * MODEL_NOISE;
+
+        // Update state
+        state.x = (int) (stepX * state.x + noiseX);
+        state.y = (int) (stepY * state.y + noiseY);
+
+        // Update error
+        state.errorX = state.errorX * stepX * stepX + PREDICTION_ERROR;
+        state.errorY = state.errorY * stepY * stepY + PREDICTION_ERROR;
+    }
+
+    /**
+     * Update the model based on the newest measurement
+     *
+     * @param measurement measurement
+     */
+    public void update(Measurement measurement) {
+        // Compute Kalman gain
+        float gainX = state.errorX / (state.errorX + measurement.accuracyRating);
+        float gainY = state.errorY / (state.errorY + measurement.accuracyRating);
+
+        // Update prediction
+        state.x += gainX * (measurement.x - state.x);
+        state.y += gainY * (measurement.y - state.y);
+
+        // Update error
+        state.errorX = (1 - gainX) * state.errorX;
+        state.errorY = (1 - gainY) * state.errorY;
     }
 }
