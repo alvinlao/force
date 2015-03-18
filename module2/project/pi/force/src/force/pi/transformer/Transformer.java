6170fc5f38ccbac901d9eb661661a93de761870b
Jae <mtbnunu@gmail.com>
2015-03-18 12:01:48 -0700

    structure for transformer added

---
+++ module2/project/pi/force/src/force/pi/transformer/Transformer.java
@@ -0,0 +1,14 @@
+package force.pi.transformer;
+
+import force.pi.Point;
+import force.pi.Point3D;
+
+/**
+ * Created by Jae on 2015-03-18.
+ */
+public class Transformer {
+    public static Point transform3Dto2D(Point3D cameraPoint, Point3D point){
+        //TODO: IMPLEMENT THIS
+        return new Point();
+    }
+}
